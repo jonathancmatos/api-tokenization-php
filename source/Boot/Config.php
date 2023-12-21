8c66fbe2083946de24a81034d0a25ec0e87f4039
Jonathan Costa Matos <jonathanmatos1997@hotmail.com>
2023-12-21 16:17:48 -0300

    Adding environment variable

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,40 +1,45 @@
 <?php
 
+use Dotenv\Dotenv;
+
+$dovEnv = Dotenv::createImmutable(__DIR__."../../../");
+$dovEnv->safeLoad();
+
+
 #*-> sets
 date_default_timezone_set("America/Sao_Paulo");
 
 /**
  * DATABASE
  */
-const CONF_DATABASE = [
+define("CONF_DATABASE", [
     "driver" => "mysql",
     "host" => "localhost",
-    "database" => "db_tokenization",
-    "username" => "root",
-    "password" => "",
+    "database" => $_ENV["DEV_DATABASE"],
+    "username" => $_ENV["DEV_DB_USERNAME"],
+    "password" => $_ENV["DEV_DB_PASSWORD"],
     "charset" => "utf8",
     "collation" => "utf8_unicode_ci",
     "prefix" => ""
-];
+]);
 
 
 /**
  * PROJECT URLs
  */
-const CONF_URL_BASE = "http://localhost/api-tokenization/";
+define("CONF_URL_BASE", $_ENV["DEV_URL"]);
 
 
 /**
  * PASSWORDS
  */
 const CONF_PASSWD_MIN_LEN = 8;
-const CONF_PASSWD_OPTION = ["cost" => 10];
 
 
 /**
  * TOKEN KEY
  */
-const KEY_SECRETED_TOKEN = "7DF21572CF63C49E7145DE3E6E7B5";
+define("KEY_SECRETED_TOKEN", $_ENV["API_SECRET_TOKEN"]);
 const ACCESS_TOKEN_EXPIRY = 120; //2 minutes
 const REFRESH_TOKEN_EXPIRY = 86400 ; //1 day
 
